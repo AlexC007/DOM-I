e1d71b5c3aaf52a52cc915c1d43a267b2c0b3801
Chawn Christian <chawn.christian@gmail.com>
2019-09-07 09:42:01 -0400

    Basic styling is complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,56 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const codeSnip= document.querySelector("#cta-img");
+codeSnip.src = "img/header-img.png";
+
+
+let navMenu = document.querySelectorAll('a');
+navMenu[0].textContent = siteContent['nav']['nav-item-1'];
+navMenu[1].textContent = siteContent['nav']['nav-item-2'];
+navMenu[2].textContent = siteContent['nav']['nav-item-3'];
+navMenu[3].textContent = siteContent['nav']['nav-item-4'];
+navMenu[4].textContent = siteContent['nav']['nav-item-5'];
+navMenu[5].textContent = siteContent['nav']['nav-item-6'];
+
+const dom= document.querySelector('h1');
+dom.textContent = siteContent['cta']['h1'];
+dom.style.width= '11rem';
+dom.style.textAlign= 'center';
+dom.style.marginRight= "5rem"
+const ctaBox= document.querySelector('button');
+ctaBox.textContent = siteContent['cta']['button'];
+ctaBox.style.marginRight="2rem";
+let mainh4 = document.querySelectorAll('h4');
+mainh4[0].textContent = siteContent['main-content']['features-h4'];
+mainh4[1].textContent = siteContent['main-content']['about-h4'];
+mainh4[2].textContent = siteContent['main-content']['services-h4'];
+mainh4[3].textContent = siteContent['main-content']['product-h4'];
+mainh4[4].textContent = siteContent['main-content']['vision-h4'];
+
+let mainP= document.querySelectorAll('p');
+mainP[0].textContent = siteContent['main-content']['features-content'];
+mainP[1].textContent = siteContent['main-content']['about-content'];
+mainP[2].textContent = siteContent['main-content']['services-content'];
+mainP[3].textContent = siteContent['main-content']['product-content'];
+mainP[4].textContent = siteContent['main-content']['vision-content'];
+
+
+const midImg= document.querySelector("#middle-img");
+midImg.src = "img/mid-page-accent.jpg";
+
+let contacth4 = document.querySelectorAll('h4');
+contacth4[5].textContent = siteContent['contact']['contact-h4'];
+
+let contactP= document.querySelectorAll('p');
+contactP[5].textContent = siteContent['contact']['address'];
+
+contactP[6].textContent = siteContent['contact']['phone'];
+contactP[7].textContent = siteContent['contact']['email'];
+
+let footerP= document.querySelectorAll('p');
+footerP[8].textContent = siteContent['footer']['copyright'];
+
+const contact = document.querySelector('.contact');
+contact.style.width = '9rem';
